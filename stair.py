e8534b927c597604027f02fe7260c47284944db2
Oleg Ushakov <olegushakov34@gmail.com>
2026-06-11 15:07:42 +0200

    Update stair.py

--- stair.py
+++ stair.py
@@ -198,7 +198,7 @@ with col_main:
     """
 
     st.subheader("Side View Drawing")
-    st.components.v1.html(svg_wrapper, height=svg_h + 20)
+    st.html(svg_wrapper, height=svg_h + 20)
 
     if blondel_applies:
         st.info(
