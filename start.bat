2cbb2e8104afea862382da80d9ee0cdc2b47718a
Oleg Ushakov <olegushakov38@gmail.com>
2026-07-21 12:11:52 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,5 @@
 @echo off
-echo Starting ISO 14122-3 Stair Calculator...
-echo.
+cd /d "%~dp0"
 
 if not exist venv\Scripts\activate.bat (
     echo Virtual environment not found. Run install.bat first.
