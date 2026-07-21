4045d58c117a365d9e9bf5703b702191f0147f58
Oleg Ushakov <olegushakov38@gmail.com>
2026-07-21 12:21:41 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 @echo off
 cd /d "%~dp0"
-call venv\Scripts\activate
-streamlit run stair.py --server.headless true
+echo Starting ISO 14122-3 Stair Calculator...
+echo.
+venv\Scripts\streamlit.exe run stair.py --server.headless true
 pause
