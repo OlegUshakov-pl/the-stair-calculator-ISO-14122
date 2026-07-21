412f224d5f6fd3eb64ee8088c1bb041f81bf6a0d
Oleg Ushakov <olegushakov38@gmail.com>
2026-07-21 12:14:31 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,11 +1,4 @@
 @echo off
 cd /d "%~dp0"
-
-if not exist venv\Scripts\activate.bat (
-    echo Virtual environment not found. Run install.bat first.
-    pause
-    exit /b 1
-)
-
 call venv\Scripts\activate.bat
 streamlit run stair.py --server.headless true
